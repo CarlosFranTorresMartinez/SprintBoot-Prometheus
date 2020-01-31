1d5bc96ce8f37492aeaf94db70e39e4ae1474b8f
Luis De la Cruz <ldelacruz@vallegrande.edu.pe>
2020-01-31 17:25:58 -0500

    Creo q funciona

--- micrometer/src/main/java/com/carlos/micrometer/micrometersprintboot15/HelloController.java
+++ micrometer/src/main/java/com/carlos/micrometer/micrometersprintboot15/HelloController.java
@@ -1,13 +1,21 @@
 package com.carlos.micrometer.micrometersprintboot15;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index(){
-        return "Biendo a Spring Boot";
+    @Autowired
+    MeterRegistry meterRegistry;
+
+    @GetMapping
+    public String prueba() {
+        meterRegistry.counter("Metrica")
+                .increment();
+        return "MetricaHola";
     }
 }
